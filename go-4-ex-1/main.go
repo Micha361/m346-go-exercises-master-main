351c7b42267e6c204a2d74b8a7865240bcda66a7
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2024-12-13 15:29:37 +0100

    schluss comit

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,20 @@
 package main
 
-// TODO: implement the function computeGrade
+func computeGrade(gotPoints, maxPoints float64) float64 {
+	if maxPoints <= 0 {
+		panic("maxPoints must be greater than zero")
+	}
+	note := 1.0 + 5.0*(gotPoints/maxPoints)
+	if note > 6.0 {
+		return 6.0
+	} else if note < 1.0 {
+		return 1.0
+	}
+	return note
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	Println(computeGrade(17.5, 28.0)) 
+	Println(computeGrade(25.0, 30.0)) 
+	Println(computeGrade(10.0, 40.0)) 
 }
