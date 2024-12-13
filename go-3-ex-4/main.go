87c9d9b98b7d5a48a2e7ea0e691c51f012ad0b1a
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2024-12-13 14:58:30 +0100

    go 3 aufgaben (die ersten 3)

--- go-3-ex-4/main.go
+++ go-3-ex-4/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 const (
-	Diamonds = '\u25c6' // Karo
-	Spades   = '\u2660' // Pik
-	Clubs    = '\u2663' // Kreuz
-	Hearts   = '\u2665' // Herz
+	Diamonds = '\u25c6' 
+	Spades   = '\u2660'
+	Clubs    = '\u2663' 
+	Hearts   = '\u2665'
 
 	Six   = '\u2465'
 	Seven = '\u2466'
@@ -23,8 +23,6 @@ func main() {
 	suits := []rune{Diamonds, Spades, Clubs, Hearts}
 	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
 
-	// TODO: Loop over suits and ranks to output all combinations.
 
-	// TODO: delete this line afterwards
 	fmt.Println(suits, ranks)
 }
