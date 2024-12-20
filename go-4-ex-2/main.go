2487aaed13dc677ff5a7809a3342506ef2ad92a7
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2024-12-20 14:23:10 +0100

    lösung 2ter Aufgabe von 4

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,16 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+func computeHypotenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(3, 4))  
+	fmt.Println(computeHypotenuse(5, 12)) 
+	fmt.Println(computeHypotenuse(8, 15)) 
 }
